0f2ec74b482ebbddfd914a8a191762db8cdfa686
ProgramInc <43065474+ProgramInc@users.noreply.github.com>
2019-04-13 04:19:40 +0300

    Got rid of dummy data and now using MySQL

--- run.py
+++ run.py
@@ -1,92 +1,34 @@
 from flask import Flask, render_template, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://zem:Zemer123!@localhost/store'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-products = [
-    {
-        'id': '0',
-        'brand': 'Brand Name 1',
-        'name': 'Product Name 1',
-        'description': 'This is the description for product 1',
-        'price': '10',
-        'units_sold': '0'
+db = SQLAlchemy(app)
 
-    },
-    {
-        'id': '1',
-        'brand': 'Brand Name 2',
-        'name': 'Product Name 2',
-        'description': 'This is the description for product 2',
-        'price': '20',
-        'units_sold': '1'
-    },
-    {
-        'id': '2',
-        'brand': 'Brand Name 3',
-        'name': 'Product Name 3',
-        'description': 'This is the description for product 3',
-        'price': '7',
-        'units_sold': '2'
 
-    },
-    {
-        'id': '3',
-        'brand': 'Brand Name 2',
-        'name': 'Product Name 4',
-        'description': 'This is the description for product 4',
-        'price': '30',
-        'units_sold': '3'
-    },
-    {
-        'id': '4',
-        'brand': 'Brand Name 3',
-        'name': 'Product Name 5',
-        'description': 'This is the description for product 5',
-        'price': '2',
-        'units_sold': '4'
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    brand = db.Column(db.String(30), nullable=False)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    description = db.Column(db.String(100), unique=True, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    units_sold = db.Column(db.Integer, nullable=False)
 
-    },
-    {
-        'id': '5',
-        'brand': 'Brand Name 4',
-        'name': 'Product Name 6',
-        'description': 'This is the description for product 6',
-        'price': '50',
-        'units_sold': '5'
-    },
-    {
-        'id': '6',
-        'brand': 'Brand Name 1',
-        'name': 'Product Name 7',
-        'description': 'This is the description for product 7',
-        'price': '9',
-        'units_sold': '6'
-
-    },
-    {
-        'id': '7',
-        'brand': 'Brand Name 2',
-        'name': 'Product Name 8',
-        'description': 'This is the description for product 8',
-        'price': '29',
-        'units_sold': '7'
-    },
-    {
-        'id': '8',
-        'brand': 'Brand Name 4',
-        'name': 'Product Name 9',
-        'description': 'This is the description for product 8',
-        'price': '35',
-        'units_sold': '8'
-    }
-    ]
+    def __repr__(self):
+        return f"'id': '{self.id}', 'brand': '{self.brand}', 'name': '{self.name}'," \
+               f" 'descripstion':'{self.description}', 'price': '{self.price}', 'units_sold': '{self.units_sold}'"
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    data = Product.query.all()
+    products = []
+    for row in data:
+        products.append({row})
     return render_template('home.html', products=products, title='Home')
 
 
@@ -102,8 +44,8 @@ def contact():
 
 @app.route("/product/<int:product_id>")
 def product_page(product_id):
-    product = products[product_id]
-    return render_template('product.html', title=(products[product_id])['name'], product=product)
+    product= Product.query.get_or_404(product_id)
+    return render_template('product.html', title=product.name, product=product)
 
 
 if __name__ == '__main__':
